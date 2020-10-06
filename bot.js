71bd3cd8d1cd92e1a9f7d35b1200af83406b2c38
NOVAGLITCHER <72463753+NOVAGLITCHER@users.noreply.github.com>
2020-10-06 19:58:05 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+
+const Client = new Discord.Client;
+
+Client.on("ready", () → (
+console.log("bot operationnel");
+
+ent.login("NzYzMDYyNjY4MzQ0MDMzMjgw.X3yPfQ.t-b9A6Okv_Oi0Uhl6dAQhk-ghys");
